37c7d69dd639b553007a4ffadaf28fcc9364ab8f
palle torsson <palle.torsson@gmail.com>
2013-04-11 23:39:11 +0200

    adding some regexp for google and duckduckgo

--- ungoogle.py
+++ ungoogle.py
@@ -2,6 +2,7 @@
 import sys
 import serial
 import datetime
+import re
 
 run_serial = False # turn off/on serial to console
 printing = False # turn off/on print to console
@@ -30,23 +31,39 @@ def check_google():
 
         try:
             if (google_request):
-                activate_to_indicator()
+                activate_to_indicator(google_request)
         except:
             print 'Could not activate google indicator, you might need to connnect the Arduino'
 
 
 # ping the Arduino and delay next indicator window with 3 seconds
-def activate_to_indicator():
+def activate_to_indicator(google_request):
     global time
     time2 = datetime.datetime.now()
     diff = time2 - time
     if diff > datetime.timedelta(seconds=1):
         time = time2
         formated_time = time2.strftime('%H:%M:%S')
-        print " [ %s ] don't be evil" % formated_time
         # the Arduino is listing to for "g"
-        # serial.write("g")
+        regex_line(google_request, formated_time )
 
+
+
+def regex_line(line, formated_time ):
+    google  = re.search(r'1e100', line)
+    duckduck = re.search(r'ec2-46-51-197-88', line)
+
+    if google:
+        f = 'goog'
+        print " [ %s ] don't be evil" % formated_time
+        # serial.write("g")
+    elif duckduck:
+        f = 'duck'
+        print " [ %s ] duck duck " % formated_time
+        # serial.write("d")
+    else:
+        f = 'no match'
+    return f
 
 
 if __name__ == '__main__':
--- ungoogle.sh
+++ ungoogle.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-sudo tcpdump -i wlan0 host google.com  | python ungoogle.py
+sudo tcpdump -i wlan0 host google.com or dst 46.51.197.89 or dst 46.51.197.88 | python ungoogle.py
