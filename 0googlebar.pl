0914869b7b0101ba6b8af6ea5e93ee1ffbf92b92
Pontus Walck <pontus@walck.se>
2013-04-13 20:04:44 +0300

    Update 0googlebar.pl
    Updated to read and send characters as they are added to a "send_to_arduino" file.

--- 0googlebar.pl
+++ 0googlebar.pl
@@ -10,9 +10,25 @@ my $Arduino = Device::SerialPort::Arduino->new(
   parity   => 'none',
     );
 
-# Reading from Arduino via Serial
 
-$Arduino->communicate($ARGV[0]);
+# Read characters from the end of the "send_to_arduino" file
+
+open $f, "<", "send_to_arduino" or die "touch send_to_arduino, plz";
+seek $f, 0, 2;  # Seeks to EOF.
+
+while (1)
+{
+  while (read($f, $c, 1))
+  {
+    $Arduino->communicate($c);
+    sleep(1);
+  };
+  
+  sleep(1);
+  seek $f, 0, 1; # Seeks without moving pointer, to reset EOF.
+}
+
+
 
 # while (1) {
 #   print $Arduino->receive(), "\n";
